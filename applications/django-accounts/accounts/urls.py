e2110da5e943293367856992284d5bdfb1b23eac
voronokKita <kitavoronok@gmail.com>
2022-07-25 15:22:15 +0300

    accounts template-view bones

--- applications/django-accounts/accounts/urls.py
+++ applications/django-accounts/accounts/urls.py
@@ -5,5 +5,8 @@ from . import views
 
 app_name = 'accounts'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('/', views.IndexView.as_view(), name='index'),
+    path('/register', views.RegisterView.as_view(), name='register'),
+    path('/login', views.LoginView.as_view(), name='login'),
 ]
--- applications/django-accounts/accounts/views.py
+++ applications/django-accounts/accounts/views.py
@@ -1,5 +1,19 @@
-from django.urls import reverse
+import logging
+
+from django.urls import reverse, reverse_lazy
+from django.views import generic
 
 
-def index(request):
-    return 'OK'
+logger = logging.getLogger(__name__)
+
+
+class IndexView(generic.TemplateView):
+    template_name = 'accounts/index.html'
+
+
+class RegisterView(generic.CreateView):
+    template_name = 'accounts/index.html'
+
+
+class LoginView(generic.FormView):
+    template_name = 'accounts/index.html'
