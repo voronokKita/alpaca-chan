8bf7810a776f88a01827c22ea917942f9c11371f
voronokKita <kitavoronok@gmail.com>
2022-07-26 18:11:18 +0300

    navbar

--- applications/django-cs50web-wiki/encyclopedia/views.py
+++ applications/django-cs50web-wiki/encyclopedia/views.py
@@ -1,7 +1,7 @@
 import logging
 import markdown2
 
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import generic
 
 from .models import Entry
@@ -10,10 +10,18 @@ from .forms import EntryForm, DeleteEntryForm
 logger = logging.getLogger(__name__)
 
 
+def get_default_nav():
+    return [{'url': reverse_lazy('encyclopedia:index'), 'text': 'Goto home', 'focus': True}, ]
+
+
 class IndexView(generic.ListView):
     template_name = 'encyclopedia/index.html'
     queryset = Entry.objects.order_by('entry_name')[:20]
     context_object_name = 'wiki_entries'
+    extra_context = {
+        'navbar_list': [{'url': reverse_lazy('encyclopedia:new_entry'),
+                        'text': 'Write a new article', 'focus': True}]
+        }
 
 
 class DetailView(generic.DetailView):
@@ -26,23 +34,28 @@ class DetailView(generic.DetailView):
         context['entry_text_html'] = markdown2.markdown(
             context['article'].entry_text
         )
+        slug = context['article'].slug
+        context['navbar_list'] = [
+            {'url': reverse('encyclopedia:index'), 'text': 'Go back', 'focus': True},
+            {'url': reverse('encyclopedia:edit_entry', kwargs={'slug': slug}),
+             'text': 'Edit this article'},
+            {'url': reverse('encyclopedia:delete_entry', kwargs={'slug': slug}),
+             'text': 'Delete this article'},
+        ]
         return context
 
 
 class AddNewEntry(generic.CreateView):
     template_name = 'encyclopedia/entry_form.html'
     form_class = EntryForm
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Add new article'
-        return context
+    extra_context = {'title': 'Add new article', 'navbar_list': get_default_nav()}
 
 
 class EditEntry(generic.UpdateView):
     template_name = 'encyclopedia/entry_form.html'
     form_class = EntryForm
     model = Entry
+    extra_context = {'navbar_list': get_default_nav()}
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -55,6 +68,7 @@ class DeleteEntry(generic.DeleteView):
     form_class = DeleteEntryForm
     model = Entry
     success_url = reverse_lazy('encyclopedia:index')
+    extra_context = {'navbar_list': get_default_nav()}
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
--- applications/django-polls/polls/views.py
+++ applications/django-polls/polls/views.py
@@ -1,14 +1,16 @@
-import logging
-
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 
 from django.utils import timezone
 from django.db.models import F
 
 from .models import Question, Choice
+
+
+def get_default_nav():
+    return [{'url': reverse_lazy('polls:index'), 'text': 'Goto home', 'focus': True}, ]
 
 
 class IndexView(generic.ListView):
@@ -24,6 +26,7 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     template_name = 'polls/detail.html'
     model = Question
+    extra_context = {'navbar_list': get_default_nav()}
 
     def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.localtime())
@@ -32,6 +35,7 @@ class DetailView(generic.DetailView):
 class ResultsView(generic.DetailView):
     template_name = 'polls/results.html'
     model = Question
+    extra_context = {'navbar_list': get_default_nav()}
 
     def get_queryset(self):
         return Question.objects.filter(pub_date__lte=timezone.localtime())
@@ -40,11 +44,11 @@ class ResultsView(generic.DetailView):
 def vote(request, question_pk):
     q = get_object_or_404(Question, pk=question_pk)
     try:
-        selected = q.choices.get(pk=request.POST['choice'])
+        selected = q.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(
             request, 'polls/detail.html',
-            {'q': q, 'error_message': "You didn't select a choice."}
+            {'question': q, 'error_message': "You didn't select a choice.", 'navbar_list': get_default_nav()}
         )
     else:
         selected.votes = F('votes') + 1
